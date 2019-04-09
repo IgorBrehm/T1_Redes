6b488a931390b18fc635ce2e3b8ab466d54d647e
Igor Brehm <igorbrehm@hotmail.com>
2019-04-09 20:31:07 -0300

    codigo bosta

--- TCPClient.java
+++ TCPClient.java
@@ -5,33 +5,43 @@ import java.util.Scanner;
 class TCPClient {
 	public static void main(String args[]) throws Exception {
 	  	Scanner input = new Scanner (System.in);
-        ServerSocket serverSocket = new ServerSocket(6788);
         String serverSentence;
-		String message;
 		String name;
         Socket clientSocket = new Socket("localhost", 6789);
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
         
-        Socket connectionSocket = welcomeSocket.accept();
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
 		boolean flag = false;
 		while (flag == false) {
             System.out.println("Digite seu nome:");
             name = input.nextLine();
             outToServer.writeBytes("Name-"+name);
+            ServerSocket serverSocket = new ServerSocket(6788);
+            Socket connectionSocket = serverSocket.accept();
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
 			serverSentence = inFromServer.readLine();
             if(serverSentence.equals("Iniciando jogo")){
-                // comeca o jogo
+                flag = true;
             }
             else if(serverSentence.equals("Aguardando segundo jogador")){
-                // congela e fica esperando
+                System.out.println("Aguardando o seu adversario chegar!");
+                while(true){
+                    serverSentence = inFromServer.readLine();
+                    if(serverSentence.equals("Iniciando jogo")){
+                        flag = true;
+                        break;
+                    }
+                }
             }
             else{
-                // nome invalido, pede pra mandar de novo
+                System.out.println("Nome Invalido! Tente novamente.");
             }
 	    }
         
-        // joga o jogo    
+        flag = false;
+        while(flag == false){
+            flag = false;
+            // TODO execucao das rodadas
+        }
         clientSocket.close();
 	    input.close();
 	    System.exit(0);
--- TCPServer.java
+++ TCPServer.java
@@ -5,44 +5,45 @@ import java.time.*;
 class TCPServer {
 	public static void main(String args[]) throws Exception {
 		String clientSentence;
-        String message;
         String[] players = new String[2];
         players[0] = "";
         players[1] = "";
-        Socket outSocket = new Socket("localhost", 6788);
-        DataOutputStream outToClient = new DataOutputStream(outSocket.getOutputStream());
+        int[] scores = new int[]{0,0};
         int count = 0;
 		ServerSocket serverSocket = new ServerSocket(6789);
-		while (true) {
-            while(players[0].equals("") && players[1].equals("")){
-                Socket connectionSocket = welcomeSocket.accept();
-                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-                clientSentence = inFromClient.readLine();
-                String[] data = clientSentence.split("-", 2);
-                if(data[0].equals("Name")){
-                    if(data[1].equals("")){
-                        outToClient.writeBytes("Nome invalido");
-                    }
-                    else{
-                        players[count] = data[1];
-                        if(count == 0){
-                            outToClient.writeBytes("Aguardando segundo jogador");
-                            count = count + 1;
-                        }
-                        else{
-                            outToClient.writeBytes("Iniciando jogo");
-                        }
-                    }
+        Socket connectionSocket = serverSocket.accept();
+        BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+        
+        while(players[0].equals("") && players[1].equals("")){
+            clientSentence = inFromClient.readLine();
+            String[] data = clientSentence.split("-", 2);
+            Socket outSocket = new Socket("localhost", 6788);
+            DataOutputStream outToClient = new DataOutputStream(outSocket.getOutputStream());
+            if(data[0].equals("Name")){
+                if(data[1].equals("")){
+                    outToClient.writeBytes("Nome invalido");
                 }
                 else{
-                    outToClient.writeBytes("Envie seu nome primeiro");
+                    players[count] = data[1];
+                    if(count == 0){
+                        outToClient.writeBytes("Aguardando segundo jogador");
+                        count = count + 1;
+                    }
+                    else{
+                        outToClient.writeBytes("Iniciando jogo");
+                    }
                 }
             }
-			count = 0;
-            // ja tenho os nomes, agora inicio o jogo...
-            // TODO resto do programa
-			outToClient.writeBytes(message);
-			outSocket.close();
-		}
+            else{
+                outToClient.writeBytes("Envie seu nome primeiro");
+            }
+            outSocket.close();
+        }
+        count = 0;
+        while(count < 3){
+            clientSentence = inFromClient.readLine();
+            // TODO execucao das rodadas
+            count = count +1;
+        }
 	}
 }
